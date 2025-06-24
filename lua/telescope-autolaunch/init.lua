45699f02b65d09d5773494c4b16e06c395902561
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2025-06-24 13:23:57 +0100

    why was it nested in two jobs?

--- lua/telescope-autolaunch/init.lua
+++ lua/telescope-autolaunch/init.lua
@@ -7,33 +7,19 @@ function M.setup()
 
 	Job:new({
 		command = "git",
-		args = { "rev-parse", "--is-inside-work-tree" },
+		args = { "status", "--porcelain" },
 		cwd = cwd,
-		on_exit = function(_, return_val)
-			if return_val ~= 0 then
+		on_exit = function(j2, _)
+			local result = j2:result()
+			if #result > 0 then
+				vim.schedule(function()
+					require("telescope.builtin").git_status()
+				end)
+			else
 				vim.schedule(function()
 					require("telescope.builtin").find_files()
 				end)
-				return
 			end
-
-			Job:new({
-				command = "git",
-				args = { "status", "--porcelain" },
-				cwd = cwd,
-				on_exit = function(j2, _)
-					local result = j2:result()
-					if #result > 0 then
-						vim.schedule(function()
-							require("telescope.builtin").git_status()
-						end)
-					else
-						vim.schedule(function()
-							require("telescope.builtin").find_files()
-						end)
-					end
-				end,
-			}):start()
 		end,
 	}):start()
 end
