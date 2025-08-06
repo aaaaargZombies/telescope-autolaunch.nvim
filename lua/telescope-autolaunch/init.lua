2053089925acfcff03e39ed604b867894d87ba6f
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2025-08-06 12:28:49 +0100

    don't run on commit message

--- lua/telescope-autolaunch/init.lua
+++ lua/telescope-autolaunch/init.lua
@@ -5,20 +5,22 @@ function M.setup()
 	local Job = require("plenary.job")
 	local cwd = vim.fn.getcwd()
 
-	Job:new({
-		cwd = cwd,
-		command = "git",
-		args = { "rev-parse", "--is-inside-work-tree" },
-		on_exit = function(j, _)
-			local result = j:result()
-			-- If inside a git repo, launch telescope.find_files
-			if #result > 0 and result[1] == "true" then
-				vim.schedule(function()
-					require("telescope.builtin").find_files()
-				end)
-			end
-		end,
-	}):start()
+	if vim.bo.filetype ~= "gitcommit" then
+		Job:new({
+			cwd = cwd,
+			command = "git",
+			args = { "rev-parse", "--is-inside-work-tree" },
+			on_exit = function(j, _)
+				local result = j:result()
+				-- If inside a git repo, launch telescope.find_files
+				if #result > 0 and result[1] == "true" then
+					vim.schedule(function()
+						require("telescope.builtin").find_files()
+					end)
+				end
+			end,
+		}):start()
+	end
 end
 
 return M
